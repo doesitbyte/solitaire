b452ed759c4e92736380823fc4b1e3d9c7f3ea1c
Kaustubh Sharma <kaustubhsharma999@gmail.com>
2024-06-21 01:46:33 +0530

    init

---
+++ src/PreInitState.ts
@@ -0,0 +1,24 @@
+import * as Phaser from "phaser";
+
+import { baseURL } from "./constants/loading";
+
+const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
+  active: false,
+  key: "PreInitState",
+  visible: false,
+};
+
+export default class PreInitState extends Phaser.Scene {
+  public constructor() {
+    super(sceneConfig);
+  }
+
+  public preload(): void {
+    // Set base url
+    this.load.baseURL = baseURL;
+  }
+
+  public create(): void {
+    this.scene.start("InitState");
+  }
+}
