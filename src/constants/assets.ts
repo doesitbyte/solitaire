e99fd72dd0eef4c21914dbe35ee709ca83db2679
Kaustubh Sharma <kaustubhsharma999@gmail.com>
2024-06-28 11:28:34 +0530

    moved shuffles to assets folder

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -12,7 +12,7 @@ export const spritesheets = [
 ];
 
 export const difficulties = [
-  { file: "shuffles/easy_shuffles_1.json", key: "easy_shuffles" },
-  { file: "shuffles/medium_shuffles_1.json", key: "medium_shuffles" },
-  { file: "shuffles/hard_shuffles_1.json", key: "hard_shuffles" },
+  { file: "assets/shuffles/easy_shuffles_1.json", key: "easy_shuffles" },
+  { file: "assets/shuffles/medium_shuffles_1.json", key: "medium_shuffles" },
+  { file: "assets/shuffles/hard_shuffles_1.json", key: "hard_shuffles" },
 ];
